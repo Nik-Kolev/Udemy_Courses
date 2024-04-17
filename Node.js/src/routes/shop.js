2e30d28b21b3c76fbb1fc22988dd42840f39ddc5
Nik-Kolev <141948860+Nik-Kolev@users.noreply.github.com>
2024-04-17 23:33:34 +0300

    section 9 done

--- Node.js/src/routes/shop.js
+++ Node.js/src/routes/shop.js
@@ -1,16 +1,25 @@
-const express = require("express");
+const path = require('path');
+
+const express = require('express');
+
+const shopController = require('../controllers/shop');
+
 const router = express.Router();
 
-const shopController = require("../controllers/shop");
+router.get('/', shopController.getIndex);
 
-router.get("/", shopController.getIndex);
+router.get('/products', shopController.getProducts);
 
-router.get("/products", shopController.getProducts);
+router.get('/products/:productId', shopController.getProduct);
 
-router.get("/cart", shopController.getCart);
+router.get('/cart', shopController.getCart);
 
-router.get("/checkout", shopController.getCheckout);
+router.post('/cart', shopController.postCart);
 
-router.get("/orders", shopController.getOrders);
+router.post('/cart-delete-item', shopController.postCartDeleteProduct);
+
+router.get('/orders', shopController.getOrders);
+
+router.get('/checkout', shopController.getCheckout);
 
 module.exports = router;
