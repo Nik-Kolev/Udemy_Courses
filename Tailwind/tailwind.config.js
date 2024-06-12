9d8ca67da818057e8d507df3e53c6dc1fd4ece15
Nik-Kolev <141948860+Nik-Kolev@users.noreply.github.com>
2024-06-13 00:07:37 +0300

    light and dark mode added

--- Tailwind/tailwind.config.js
+++ Tailwind/tailwind.config.js
@@ -1,25 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
-  theme: {
-    extend: {
-      keyframes: {
-        wavey: {
-          "0%, 100%": {
-            transform: "scaleY(0.5)",
-          },
-          "50%": {
-            transform: "scaleY(1.5)",
-          },
+    darkMode: "class",
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+        extend: {
+            keyframes: {
+                wavey: {
+                    "0%, 100%": {
+                        transform: "scaleY(0.5)",
+                    },
+                    "50%": {
+                        transform: "scaleY(1.5)",
+                    },
+                },
+            },
         },
-      },
+        animation: {
+            wavey: "wavey 1s linear infinite",
+        },
     },
-    animation: {
-      wavey: "wavey 1s linear infinite",
-    },
-  },
-  plugins: [
-    require("./plugins/openVariant"),
-    require("./plugins/animationDelay"),
-  ],
+    plugins: [
+        require("./plugins/openVariant"),
+        require("./plugins/animationDelay"),
+    ],
 };
